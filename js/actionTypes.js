2d990f1f78b0048231384aa2312c32704c43cceb
RishabBaisakh <rishab.baisakh@gmail.com>
2021-01-19 22:48:41 -0500

    added redux setup

---
+++ js/actionTypes.js
@@ -0,0 +1,3 @@
+export const NEW_NUMBER_GENERATED = "newNumberGenerated";
+export const MOVE_PERFORMED = "movePerformed";
+export const UNDO_PERFORMED = "undoPerformed";
---
+++ js/reducer.js
@@ -0,0 +1,8 @@
+import * as actions from "./actionTypes";
+
+initialState = {
+  numberOfMoves: 0,
+  boardState: [],
+};
+
+export default function reducer(state = [], action) {}
